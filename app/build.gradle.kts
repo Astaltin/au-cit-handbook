92a72dd2327e348c0ce071a96d736b2492593ac8
Astaltin <astaltin@gmail.com>
2024-01-14 01:40:51 +0800

    directory relocated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.au.studenthandbook"
+    namespace = "com.au.cit.handbook"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.au.studenthandbook"
+        applicationId = "com.au.cit.handbook"
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -18,7 +18,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
