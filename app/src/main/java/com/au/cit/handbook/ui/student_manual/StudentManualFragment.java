fad853e64ed313652620e540d0da2cdf61683f1d
Astaltin <astaltin@gmail.com>
2024-02-13 05:31:38 +0800

    final commit

--- app/src/main/java/com/au/cit/handbook/ui/student_manual/StudentManualFragment.java
+++ app/src/main/java/com/au/cit/handbook/ui/student_manual/StudentManualFragment.java
@@ -4,11 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.au.cit.handbook.databinding.FragmentStudentManualBinding;
 
@@ -19,7 +20,13 @@ public class StudentManualFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentStudentManualBinding.inflate(inflater, container, false);
+        WebView studentManualWebView = binding.studentManualWebView;
 
+        WebSettings webSettings = studentManualWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        studentManualWebView.setWebViewClient(new WebViewClient());
+        studentManualWebView.loadUrl("https://drive.google.com/file/d/1zIfG7y-t_SiBlk679yFOetmrgZ14WI85/view?usp=sharing");
         return binding.getRoot();
     }
 
